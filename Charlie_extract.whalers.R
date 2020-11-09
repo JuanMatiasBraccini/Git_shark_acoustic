73e485272f6690b29c2d58656439efe165215e9c
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2020-11-09 15:01:10 +0800

    added a few new tags detected in SA

--- Charlie_extract.whalers.R
+++ Charlie_extract.whalers.R
@@ -1,4 +1,6 @@
 #Charlie's whaler sharks
+library(data.table)
+library(dplyr)
 
 setwd("C:/Matias/Data/Tagging/Acoustic_tagging/Acoustic_tagging_data")
 Detections=fread("Detections.csv",data.table=FALSE)
@@ -10,12 +12,27 @@ Detections=fread("Detections.csv",data.table=FALSE)
 # SMN.bronzey=read.csv(file="SMN_donwloads_processed/copper.2018-10-19.csv")
 
 South.Oz=read.csv(file="C:/Matias/Data/Tagging/Acoustic_tagging/Other researcher's tags/Charlie's tags.csv")
-These.tags=c(South.Oz$Code2,23303, 27698, 31003,29542,
-                 30992, 29587, 30891, 26438, 30894, 31003, 31000)
+These.tags=c(South.Oz$Code2)
+WA.tagged_detected.in.SA=c(23293,23294,23303,26438,27698,29542,29454,29587,30717,30870,30891,30894,30992,31000,31003)
+These.tags=c(These.tags,WA.tagged_detected.in.SA)
 
+
+  #tags
+TAGS=read.csv("TAGS.csv",stringsAsFactors=F)%>%
+  filter(Species2%in%c('bronze whaler','Dusky') & Project.rel=="SMN")
+SMN.tags.bronze.dusky=Detections%>%
+                      filter(Species%in%c('bronze whaler','Dusky') & Project=="SMN")%>%
+                      distinct(TagCode,.keep_all = T)
+
+write.csv(unique(c(TAGS$Code2,SMN.tags.bronze.dusky$TagCode)),"C:/Matias/Analyses/Acoustic_tagging/For Charlie/Data/For.Charlie_WA.tags.csv",row.names=F)
+
+
+cc=c(23293,26438,30717)
+subset(TAGS,Code2%in%cc)
+
+  #detections
 SMN=subset(Detections,Species%in%c('bronze whaler','Dusky'))
 SMN$Sex=as.character(SMN$Sex)
 SMN$Sex=ifelse(SMN$Sex=="m","M",SMN$Sex)
 SMN=subset(SMN,TagCode%in%These.tags & Longitude<=129)
-
 write.csv(SMN,"C:/Matias/Analyses/Acoustic_tagging/For Charlie/Data/For.Charlie.csv",row.names=F)
